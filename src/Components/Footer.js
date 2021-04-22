f4609fb2405dcfcdff34add82536d72d041a8637
Akshay <akshaypooloth1@gmail.com>
2021-04-22 23:40:00 +0530

    added: Insta Link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,15 +5,15 @@ const Footer= ()=>{
     <footer className="footer d-flex justify-content-between align-items-center px-2">
     <span className="text-white f-5 fw-bold fs-7">coded with ❤️ in react.js </span>
     <div className="">
-        <a target="_blank" className="text-decoration-none" href="https://github.com/akshayitzme/blueweather-v3">
+        <a className="text-decoration-none" href="https://github.com/akshayitzme/blueweather-v3">
             <i className="bi fs-3 bi-github text-dark"></i>
         </a>
 
-        <a target="_blank" className="text-decoration-none" href="https://instagram.com/akshayitzme">
+        <a className="text-decoration-none" href="https://instagram.com/akshayitzme">
             <i class="clr-12 fs-3 bi bi-instagram"></i>    
         </a> 
         
-        <a target="_blank" className="text-decoration-none" href="https://t.me/coderitzme">
+        <a className="text-decoration-none" href="https://t.me/coderitzme">
             <i className="bi bi-telegram fs-3 text-white"></i>
         </a>
     </div>
