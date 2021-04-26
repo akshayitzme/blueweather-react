4b680531bd1cc325e069f230b046d074fcf45f88
Akshay <akshaypooloth1@gmail.com>
2021-04-26 19:09:34 +0530

    dark mode fixes

--- src/Components/HumidityCard.js
+++ src/Components/HumidityCard.js
@@ -11,14 +11,14 @@ const HumidityCard= (props)=>{
                   
                   
                   <div className="d-flex align-items-center h-auto clr-12 bg-10 border border-2 border-warning rounded p-1 py-2">
-                      <img src={HumidIcon} className="w-Icon" alt="" srcset=""/>
+                      <img src={HumidIcon} className="w-Icon" alt="HumidityIcon" srcset=""/>
                       <span className="fs-1">{props.humid}</span>
                       <span>%</span> 
                   </div>
                   <div className="d-flex flex-column justify-content-center ">
-                      <button href="#" data-toggle="tooltip" data-placement="top" title="Precipitation is button form of water from the atmosphere" className="border-bottom border-2 border-warning clr-31 fw-bold text-decoration-none">Precipitation: <span className="clr-4">{props.preci} mb</span></button>
-                      <button href="#" data-toggle="tooltip" data-placement="top" title="Visibility is button measure of the distance at which an object or light can be clearly discerned." className="border-bottom border-2 border-warning clr-31 fw-bold text-decoration-none">Visibiltiy: <span className="clr-4">{props.vis} k.m</span></button>
-                      <button href="#" data-toggle="tooltip" data-placement="top" title="The dew point is the temperature the air needs to be cooled to (at constant pressure) in order to achieve button relative humidity (RH) of 100%" className="border-bottom border-2 border-warning clr-31 fw-bold text-decoration-none">Dew Point: <span className="clr-4">{props.dpt} ℃</span></button>
+                      <button className="bg-transparent border-bottom border-0  border-warning clr-31 fw-bold text-decoration-none">Precipitation: <span className="text-white">{props.preci} mb</span></button>
+                      <button  className="bg-transparent border-bottom border-0  border-warning clr-31 fw-bold text-decoration-none">Visibiltiy: <span className="text-white">{props.vis} k.m</span></button>
+                      <button  className="bg-transparent border-bottom border-0  border-warning clr-31 fw-bold text-decoration-none">Dew Point: <span className="text-white">{props.dpt} ℃</span></button>
                     </div>
               </div>
         </div>
