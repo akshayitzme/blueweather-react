f79645df9c4e4c90c146805ff1f4ee28c8a734fa
Akshay <akshaypooloth1@gmail.com>
2021-04-26 18:51:39 +0530

    quickfix

--- src/Components/HumidityCard.js
+++ src/Components/HumidityCard.js
@@ -16,9 +16,9 @@ const HumidityCard= (props)=>{
                       <span>%</span> 
                   </div>
                   <div className="d-flex flex-column justify-content-center ">
-                      <button href="#" data-toggle="tooltip" data-placement="top" title="Precipitation is a form of water from the atmosphere" className="border-bottom border-2 border-warning clr-31 fw-bold text-decoration-none">Precipitation: <span className="clr-4">{props.preci} mb</span></button>
-                      <button href="#" data-toggle="tooltip" data-placement="top" title="Visibility is a measure of the distance at which an object or light can be clearly discerned." className="border-bottom border-2 border-warning clr-31 fw-bold text-decoration-none">Visibiltiy: <span className="clr-4">{props.vis} k.m</span></button>
-                      <button href="#" data-toggle="tooltip" data-placement="top" title="The dew point is the temperature the air needs to be cooled to (at constant pressure) in order to achieve a relative humidity (RH) of 100%" className="border-bottom border-2 border-warning clr-31 fw-bold text-decoration-none">Dew Point: <span className="clr-4">{props.dpt} ℃</span></button>
+                      <a href="#" data-toggle="tooltip" data-placement="top" title="Precipitation is a form of water from the atmosphere" className="border-bottom border-2 border-warning clr-31 fw-bold text-decoration-none">Precipitation: <span className="clr-4">{props.preci} mb</span></a>
+                      <a href="#" data-toggle="tooltip" data-placement="top" title="Visibility is a measure of the distance at which an object or light can be clearly discerned." className="border-bottom border-2 border-warning clr-31 fw-bold text-decoration-none">Visibiltiy: <span className="clr-4">{props.vis} k.m</span></a>
+                      <a href="#" data-toggle="tooltip" data-placement="top" title="The dew point is the temperature the air needs to be cooled to (at constant pressure) in order to achieve a relative humidity (RH) of 100%" className="border-bottom border-2 border-warning clr-31 fw-bold text-decoration-none">Dew Point: <span className="clr-4">{props.dpt} ℃</span></a>
                     </div>
               </div>
         </div>
