28bc6fbd739ae5d01c86a78403bcc99439d1cbb0
Akshay <60477442+akshayitzme@users.noreply.github.com>
2021-05-20 23:15:45 +0530

    quickfix

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,9 +8,9 @@ const Footer= ()=>{
         <a className="text-decoration-none" href="https://github.com/akshayitzme/blueweather-v3">
             <i className="bi fs-3 bi-github clr-4"></i>
         </a>
-        <a className="text-decoration-none" href="https://github.com/akshayitzme/blueweather-v3">
-            <i className="clr-4 fs-3 bi bi-instagram"></i>
-        </a>
+        {/* <a className="text-decoration-none" href="https://github.com/akshayitzme/blueweather-v3">
+            <i className="clr-4 fs-3 bi bi-instagram"></i> 
+        </a> */}
         <a className="text-decoration-none" href="https://t.me/coderitzme">
             <i className="bi bi-telegram fs-3 clr-4"></i>
         </a>
