676fbba5a73d86080f6e14e3242e25cb85482fe8
Akshay <60477442+akshayitzme@users.noreply.github.com>
2023-08-31 23:48:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ function App() {
     } else {
       if (!isLoading) setLoading(1);
       // const apikey = "e210c112d80b41728f2335ac11bcd82b";
-      // const apikey = "c969e638fd434d9d9c18117f3be2429c";
+      const apikey = "c969e638fd434d9d9c18117f3be2429c";
       await axios
         .get(
           `https://api.weatherbit.io/v2.0/current?city=${city}&key=${apikey}`
